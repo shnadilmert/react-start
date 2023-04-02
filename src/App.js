f58d60b8e6da7c7ce63e28c8a2fe4cadcc22233f
Adil Mert ŞAHİN <adil.m.sahin@hotmail.com>
2023-04-02 15:17:46 +0300

    for kullanımı htmlfor olarak değişti, class kullanımı className olarak değişiyor

--- src/App.js
+++ src/App.js
@@ -5,20 +5,24 @@ import './style.scss'
 function App() {
 
   const todos=['todo1','todo2','todo3']
+  /*
   const h1= createElement('h1',null,'adilmertsahin.com')
   const ul=createElement('ul',null,todos.map(todo=>createElement('li',null,todo)))
   return createElement( 'main',{
     className: 'test',
     id: 'main'
   }, h1,ul)
-
+*/
 
   return (
     <main>
       <h1>adilmertsahin.com</h1>
+
+      <label htmlFor='search'>Ara</label>
+      <input type='text' id='search'></input>
       <ul>
-        {todos.map(todo=>
-          <li>{todo}</li>)
+        {todos.map((todo,index)=>
+          <li key={index}>{todo}</li>)
           }
       </ul>
     </main>
