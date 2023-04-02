0ce6b018510f5d57adcd47e7fd10300cc77fdf80
Adil Mert ŞAHİN <adil.m.sahin@hotmail.com>
2023-04-03 02:17:21 +0300

    Components, children

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
-import { createElement } from 'react';
+import { createElement,useState } from 'react';
 
-import './style.scss'
+
+import Button from './components/Button';
+import Tab from './components/Tab';
+
+import './tailwind.css'
+
+function Profile(){
+  return(
+    <div>Burası profile tabı</div>
+  )
+}
 
 function App() {
 
   const todos=['todo1','todo2','todo3']
+
+  const [activeTab,setActiveTab]=useState(1)
+  
+
   /*
   const h1= createElement('h1',null,'adilmertsahin.com')
   const ul=createElement('ul',null,todos.map(todo=>createElement('li',null,todo)))
@@ -18,6 +32,22 @@ function App() {
     <main>
       <h1>adilmertsahin.com</h1>
 
+      <button onClick={()=> setActiveTab(2)}> Aktif Tabi Değiştir</button>
+
+    <div style={{padding:20}}>
+    <Tab activeTab={activeTab} onChange={tabIndex => setActiveTab(tabIndex)} >
+        <Tab.Panel title="Profil"><Profile></Profile></Tab.Panel>
+        <Tab.Panel title="Hakkında"></Tab.Panel>
+        <Tab.Panel title="Ayarlar"></Tab.Panel>
+
+      </Tab>
+      {activeTab == 2 && (
+        <div>burası da ekstra bir alan</div>
+      )}
+    </div>
+
+
+      <Button text="Merhaba Button"></Button>
       <label htmlFor='search'>Ara</label>
       <input type='text' id='search'></input>
       <ul>
---
+++ src/components/Button.js
@@ -0,0 +1,7 @@
+
+function Button(props){
+    return(
+        <button className="p-4 ">{props.text}</button>
+    );
+}
+export default Button
